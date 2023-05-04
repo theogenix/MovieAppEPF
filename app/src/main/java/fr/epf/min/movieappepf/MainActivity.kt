1ca9afc8fb20f04ba89e753c89a9196535dc7dee
theo <theogenix@gmail.com>
2023-05-05 01:03:53 +0200

    ajout couleur/police/image// premier fragment sur homePage

--- app/src/main/java/fr/epf/min/movieappepf/MainActivity.kt
+++ app/src/main/java/fr/epf/min/movieappepf/MainActivity.kt
@@ -2,10 +2,18 @@ package fr.epf.min.movieappepf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.epf.min.movieappepf.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //injecter le fragment ds la boite fragment_container
+
+        val transaction=supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container,HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
