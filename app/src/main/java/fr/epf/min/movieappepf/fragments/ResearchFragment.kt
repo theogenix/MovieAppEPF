4eaf6bcb5e0b8b45ccbbcce9ca9a4b84fe31f7f1
theo <theogenix@gmail.com>
2023-05-17 01:01:42 +0200

    ajout retrofit/ interface pour communiquer avec API/ étape 1

--- app/src/main/java/fr/epf/min/movieappepf/fragments/ResearchFragment.kt
+++ app/src/main/java/fr/epf/min/movieappepf/fragments/ResearchFragment.kt
@@ -1,6 +1,8 @@
 package fr.epf.min.movieappepf.fragments
 
+
 import android.os.Bundle
+import android.telecom.Call
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,30 +10,27 @@ import androidx.fragment.app.Fragment
 import fr.epf.min.movieappepf.MainActivity
 import fr.epf.min.movieappepf.R
 import androidx.appcompat.widget.SearchView
+import fr.epf.min.movieappepf.SearchResult
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 
-/*
-class ResearchFragment (
-    private val context: MainActivity
-        ) : Fragment(){
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view=inflater?.inflate(R.layout.fragment_research,container,false)
 
-        // recuperer search view
 
-        val searchView = view?.findViewById<SearchView>(R.id.searchView)
-        return view
-    }
-}
-*/
 
 class ResearchFragment : Fragment() {
 
     private lateinit var searchView: SearchView
+
+    // déclaration d'interface de service pour communiquer avec l'API
+    interface MovieApiService {
+        @GET("search/movie")
+        fun searchMovies(
+            @Query("api_key") apiKey: String,
+            @Query("query") query: String
+        ): retrofit2.Call<SearchResult>
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -58,3 +57,24 @@ class ResearchFragment : Fragment() {
         return view
     }
 }
+
+
+
+/*
+class ResearchFragment (
+    private val context: MainActivity
+        ) : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view=inflater?.inflate(R.layout.fragment_research,container,false)
+
+        // recuperer search view
+
+        val searchView = view?.findViewById<SearchView>(R.id.searchView)
+        return view
+    }
+}
+*/
